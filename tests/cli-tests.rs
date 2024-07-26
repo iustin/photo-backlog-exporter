966337573d0f2249b5c05dcf14ccbf575959e981
Iustin Pop <iustin@k1024.org>
2024-07-26 23:12:54 +0200

    Fix extra pipe creation in one tests
    Was wondering why I have a dangling pipe, and saw - by accident - this
    code. Not sure where it came from - probably from `mknod`-like
    invocation.

--- tests/cli-tests.rs
+++ tests/cli-tests.rs
@@ -82,7 +82,7 @@ fn test_ignores_fifo() {
     let temp_dir = tempdir().unwrap();
     let mut fname = PathBuf::from(temp_dir.path());
     fname.push("fifo.nef");
-    Command::new("mkfifo").arg(&fname).arg("p").spawn().unwrap();
+    Command::new("mkfifo").arg(&fname).spawn().unwrap();
 
     let mut cmd = Command::cargo_bin("oneshot").unwrap();
 
